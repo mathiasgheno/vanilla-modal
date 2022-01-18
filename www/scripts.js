e84bfd1eaaddd2f498208ec8480292a91eb7fc96
mathias <mathiasgheno@gmail.com>
2022-01-17 23:37:16 -0300

    chore: adiciona script com defer

--- www/scripts.js
+++ www/scripts.js
@@ -1,43 +1,40 @@
-window.onload = () => {
+const handleEscape = (event) => {
+  const isScape = event.keyCode === 27;
 
-  const handleEscape = (event) => {
-    const isScape = event.keyCode === 27;
-
-    if(!isScape) {
-      return;
-    }
-
-    const isModalOpen = document
-      .querySelector('#modal')
-      .getAttribute('class') === 'dialog-wrapper';
-
-    if(isModalOpen) {
-      handleCloseModal();
-    }
+  if(!isScape) {
+    return;
   }
 
-  const handleOpenModal = () => {
-    const modalNode = document.querySelector('#modal');
-    modalNode.setAttribute('style', 'visibility: visible;')
-    modalNode.setAttribute('class', 'dialog-wrapper');
+  const isModalOpen = document
+    .querySelector('#modal')
+    .getAttribute('class') === 'dialog-wrapper';
+
+  if(isModalOpen) {
+    handleCloseModal();
   }
-
-  const handleCloseModal = () => {
-    const modalNode = document.querySelector('#modal');
-    modalNode.setAttribute('class', 'dialog-wrapper dialog-wrapper__ease_in dialog-wrapper__closed')
-    setTimeout(() => {
-      modalNode.setAttribute('style', 'visibility: hidden;')
-    }, 210);
-  }
-
-  document
-    .querySelector('#abrir')
-    .addEventListener('click', handleOpenModal);
-
-  document
-    .querySelector('#fechar')
-    .addEventListener('click', handleCloseModal);
-
-  document
-    .addEventListener('keydown', handleEscape)
 }
+
+const handleOpenModal = () => {
+  const modalNode = document.querySelector('#modal');
+  modalNode.setAttribute('style', 'visibility: visible;')
+  modalNode.setAttribute('class', 'dialog-wrapper');
+}
+
+const handleCloseModal = () => {
+  const modalNode = document.querySelector('#modal');
+  modalNode.setAttribute('class', 'dialog-wrapper dialog-wrapper__ease_in dialog-wrapper__closed')
+  setTimeout(() => {
+    modalNode.setAttribute('style', 'visibility: hidden;')
+  }, 210);
+}
+
+document
+  .querySelector('#abrir')
+  .addEventListener('click', handleOpenModal);
+
+document
+  .querySelector('#fechar')
+  .addEventListener('click', handleCloseModal);
+
+document
+  .addEventListener('keydown', handleEscape)
